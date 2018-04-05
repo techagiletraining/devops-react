19959e2676e86c41943e26254cc3844519e8fdce
tech-agile <techagile.github@gmail.com>
2018-04-05 13:55:55 -0700

    Fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,3 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-it('fails for demo purposes', () => {
-  expect(true).toEqual(false);
-});
